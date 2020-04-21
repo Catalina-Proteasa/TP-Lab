02f4d23e8fee20928bd463b318c80e0280f80e41
Catalina-Proteasa <61597550+Catalina-Proteasa@users.noreply.github.com>
2020-04-21 12:29:48 +0300

    Add files via upload

---
+++ L7/B2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+bool prime(int n)
+{
+
+	int k = 1, d = 2;
+	while (d <= round(sqrt(n / 2)))
+	{
+		if (n%d == 0)
+			k = 0;
+		d++;
+	}
+	if (k == 1 && n != 1)
+		return true;
+	else return false;
+}
+bool circular(int n, int size)
+{
+	int co = n, a;
+	do 
+	{
+		a = co % 10;
+		co = a * pow(10, size - 1) + co / 10;
+
+		if (!prime(co))
+			return false;
+	} while (n != co);
+	return true;
+}
+int verificare()
+{
+	int i, cate = 0;
+	for (i = 2; i <= 10000; i++)
+		if (prime(i) && circular(i, ((log(i/10) + 1)))
+		{
+			cate++;
+			printf("%d\n", i);
+		}
+	if (cate != 0)
+		printf("Sunt %d numere prime circulare sub 10000", c);
+}
+int main()
+{
+	verificare();
+	system("pause");
+	return 0;
+}
